761ed96b53f84580b2282d7ad01cc4756d03c8fa
Alexander Byndyu <alexander.byndyu@gmail.com>
2013-06-02 19:44:12 +0600

    Добавил пример для рефакторинга

---
+++ RefactoringConfigExample/ConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleApp
+{
+    using System;
+    using System.Configuration;
+    using log4net;
+    using log4net.Config;
+
+    internal class Program
+    {
+        private static void Main()
+        {
+            XmlConfigurator.Configure();
+
+            ILog logger = LogManager.GetLogger("logger");
+
+            logger.WarnFormat("Some warn {0}", 109);
+
+            Console.WriteLine();
+            Console.WriteLine("SmtpHost: {0}", ConfigurationManager.AppSettings["SmtpHost"]);
+            Console.WriteLine("SmtpUserName: {0}", ConfigurationManager.AppSettings["SmtpUserName"]);
+            Console.WriteLine("Profile: {0}", ConfigurationManager.AppSettings["Profile"]);
+        }
+    }
+}
---
+++ RefactoringConfigExample/WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace WpfApp
+{
+    using System.Windows;
+    using log4net;
+    using log4net.Config;
+
+    /// <summary>
+    ///     Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            XmlConfigurator.Configure();
+
+            ILog logger = LogManager.GetLogger("logger");
+
+            logger.WarnFormat("Some warn {0}", 109);
+        }
+    }
+}
